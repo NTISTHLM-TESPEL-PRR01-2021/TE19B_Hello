a2fa653124e66bf8dc30db023b998e0745ec73be
Mikael <mikael.bergstrom@ga.ntig.se>
2020-09-03 10:46:04 +0200

    Initial commit

---
+++ Hello/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Hello
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+
+      string name;
+
+      Console.WriteLine("Skriv in ditt namn:");
+
+      // Läser in namnet från användaren
+      name = Console.ReadLine();
+
+      Console.WriteLine("Hej " + name + "!");
+
+      Console.ReadLine();
+
+    }
+  }
+}
